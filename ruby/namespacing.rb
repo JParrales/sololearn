da154a5d4d16fa339859f71c03c858eaa142e88e
JParrales <josedparrales@gmail.com>
2021-07-17 15:54:58 -0500

    namespacing

---
+++ ruby/namespacing.rb
@@ -0,0 +1,34 @@
+module Mammal
+    class Dog
+        def speak
+            puts "Woof!"
+        end
+    end
+
+    class Cat
+        def speak
+            puts "Meow"
+        end
+    end
+end
+
+a = Mammal::Dog.new
+b = Mammal::Cat.new
+
+a.speak
+b.speak
+
+module MyMath
+    PI = 3.14
+    def self.square(x)
+        x*x
+    end
+    def self.negate(x)
+        -x
+    end
+    def self.factorial(x)
+        (1..x).inject(:*) || 1
+    end
+end
+
+puts MyMath.factorial(2)
